c5fedae0f04b2592675dc98f6a00d8470600ccf0
DongHwi-Kim1 <80679060+DongHwi-Kim1@users.noreply.github.com>
2022-03-14 15:52:09 +0900

    Add files via upload

---
+++ 4a.py
@@ -0,0 +1,31 @@
+
+
+import numpy as np
+
+
+A4 = np.array([[3,0],
+               [-1,2],
+               [1,1]])
+
+
+B4 = np.array([[-3,-1],
+               [2,1],
+               [4,3]])
+
+
+C4 = np.array([[1,2,3],
+               [2,0,1]])  
+
+
+D4 = np.array([[1],
+               [2],
+               [3]])  
+
+print(A4+2*B4)
+
+print(A4.dot(C4))
+
+print(3*D4)
+
+
+
